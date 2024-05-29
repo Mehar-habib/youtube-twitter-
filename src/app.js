c1efeba5141682e6a087f637b239e59aff17c4bd
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-05-29 21:53:32 +0500

    setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 
 // Create a new instance of an Express application
 const app = new express();
@@ -12,17 +13,14 @@ app.use(
     credentials: true, // Allow cookies to be sent with requests
   })
 );
-
 // Middleware to parse incoming JSON requests with a size limit of 16kb
 app.use(express.json({ limit: "16kb" }));
-
 // Middleware to parse URL-encoded data with a size limit of 16kb
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 // Middleware to parse cookies from the HTTP request
 app.use(cookieParser());
-
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
+app.use("/api/v1/users", userRouter);
 export default app;
